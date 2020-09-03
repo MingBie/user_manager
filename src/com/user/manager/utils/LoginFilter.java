6a6a1cf1c5f91a154692f5a484ab77af7d003b70
呆自信丶 <389395995@qq.com>
2020-09-03 20:32:05 +0800

    实现基本功能(是全局刷新) 2020-9-3

--- src/com/user/manager/utils/LoginFilter.java
+++ src/com/user/manager/utils/LoginFilter.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebFilter("/*")
+// 权限设置
 public class LoginFilter implements Filter {
     public void destroy() {
     }
